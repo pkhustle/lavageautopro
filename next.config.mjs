d5d7284502a2e9415b4951780383a42e5a9476d1
Patrick Kom <komp76@gmail.com>
2025-03-10 00:11:56 -0400

    handling www 3

--- next.config.mjs
+++ next.config.mjs
@@ -11,21 +11,6 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.lavageautointerieur.ca',
-          },
-        ],
-        destination: 'https://lavageautointerieur.ca/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
